f2ee53d0da00dd3caa004ae537f243053481ff9b
Gaetan Tripelon <tripelong@gmail.com>
2024-02-16 15:11:51 +0100

    Create variables CSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      test
+      Je m'appelle Gaëtan Tripelon
+      {/* react, material ui, react-html2pdf */}
+      {/* variables css */}
+
+      {/* colonne de gauche */}
+      {/* détails personnels - User */}
+      {/* compétences - Skills */}
+      {/* langues - Skills */}
+      {/* centres d'intérêts - Interests */}
+
+      {/* colonne de droite */}
+      {/* profil - Profil */}
+      {/* formations - Formations */}
+      {/* expériences - Experiences */}
+
+      {/* light/dark mode */}
+      {/* generation PDF */}
+
     </div>
   );
 }
